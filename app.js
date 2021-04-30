a61ed04f66fb12b9678cb1b48acc6ffc676c6ef1
NahuiiGHm <nggh@hotmail.com>
2021-04-30 03:15:34 -0300

    Subiendo hasta parte 7

--- app.js
+++ app.js
@@ -2,22 +2,28 @@ const express = require("express");
 const app = express();
 const path = require ('path');
 
-app.use(express.static(path.resolve(__dirname, 'public')));
-app.use('/public/img', express.static(path.join(__dirname, '/public/img')));
+// app.use(express.static(path.resolve(__dirname, 'public')));
+// app.use('/public/img', express.static(path.join (__dirname, '/public')));
 
-app.get('/' , (req,res) => {
+const publicPath = path.resolve(__dirname, './public');
 
-    res.sendFile (path.join (__dirname + '/views/home.html'));
-});
-
-
-
-
-
+app.use (express.static(publicPath));
 
 
 
 
 app.listen('3000', function () {
     console.log('Estoy corriendo en el puerto 3000');
+});
+
+app.get('/' , (req,res) => {
+    res.sendFile (path.resolve (__dirname + '/views/home.html'));
+});
+
+app.get('/login' , (req,res) => {
+    res.sendFile (path.resolve (__dirname + '/views/login.html'));
+});
+
+app.get('/register' , (req,res) => {
+    res.sendFile (path.resolve (__dirname + '/views/register.html'));
 });
